dce86367aabf76c6baeb96b5284f286e5c0d3f49
valike23 <ludike23@gmail.com>
2023-01-22 21:33:49 +0100

    INIT

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,364 @@
+<header class="header_area header_three">
+	<!--header top start-->
+	<div class="header_top">
+		<div class="container-fluid">
+			<div class="row align-items-center">
+				<div class="col-lg-7 col-md-12">
+					<div class="welcome_text">
+						<ul>
+							<li><span>Free Delivery:</span> Take advantage of our time to save event</li>
+							<li><span>Free Returns *</span> Satisfaction guaranteed</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-5 col-md-12">
+					<div class="top_right text-right">
+						<ul>
+							<li class="top_links">
+								<a href="#">My Account <i class="ion-chevron-down" /></a>
+								<ul class="dropdown_links">
+									<li><a href="wishlist.html">My Wish List </a></li>
+									<li><a href="my-account.html">My Account </a></li>
+									<li><a href="#">Sign In</a></li>
+									<li><a href="compare.html">Compare Products </a></li>
+								</ul>
+							</li>
+							<li class="language">
+								<a href="#"
+									><img src="assets/img/logo/language.png" alt="" /> English
+									<i class="ion-chevron-down" /></a
+								>
+								<ul class="dropdown_language">
+									<li><a href="#"><img src="assets/img/logo/cigar.jpg" alt="" /> French</a></li>
+									<li><a href="#"><img src="assets/img/logo/language2.png" alt="" />German</a></li>
+								</ul>
+							</li>
+							<li class="currency">
+								<a href="#">USD <i class="ion-chevron-down" /></a>
+								<ul class="dropdown_currency">
+									<li><a href="#">EUR</a></li>
+									<li><a href="#">BRL</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--header top start-->
+
+	<!--header middel start-->
+	<div class="header_middel">
+		<div class="container-fluid">
+			<div class="middel_inner">
+				<div class="row align-items-center">
+					<div class="col-lg-4">
+						<div class="search_bar">
+							<form action="#">
+								<input placeholder="Search entire store here..." type="text" />
+								<button type="submit"><i class="ion-ios-search-strong" /></button>
+							</form>
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="logo">
+							<a href="/"><img class="logo2" src="assets/img/clothigs.png" alt="" /></a>
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="cart_area">
+							<div class="middel_links">
+								<ul>
+									<li><a href="login.html">Login</a></li>
+									<li>/</li>
+									<li><a href="login.html">Register</a></li>
+								</ul>
+							</div>
+							<div class="cart_link">
+								<a href="#"><i class="fa fa-shopping-basket" />2 item(s)</a>
+								<!--mini cart-->
+								<div class="mini_cart">
+									<div class="cart_item top">
+										<div class="cart_img">
+											<a href="#"><img src="assets/img/s-product/product.jpg" alt="" /></a>
+										</div>
+										<div class="cart_info">
+											<a href="#">Apple iPhone SE 16GB</a>
+
+											<span>1x $60.00</span>
+										</div>
+										<div class="cart_remove">
+											<a href="#"><i class="ion-android-close" /></a>
+										</div>
+									</div>
+									<div class="cart_item bottom">
+										<div class="cart_img">
+											<a href="#"><img src="assets/img/s-product/product2.jpg" alt="" /></a>
+										</div>
+										<div class="cart_info">
+											<a href="#">Marshall Portable Bluetooth</a>
+											<span> 1x $160.00</span>
+										</div>
+										<div class="cart_remove">
+											<a href="#"><i class="ion-android-close" /></a>
+										</div>
+									</div>
+									<div class="cart__table">
+										<table>
+											<tbody>
+												<tr>
+													<td class="text-left">Sub-Total :</td>
+													<td class="text-right">$150.00</td>
+												</tr>
+
+												<tr>
+													<td class="text-left">Total :</td>
+													<td class="text-right">$184.00</td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+
+									<div class="cart_button view_cart">
+										<a href="cart.html">View Cart</a>
+									</div>
+									<div class="cart_button checkout">
+										<a href="checkout.html">Checkout</a>
+									</div>
+								</div>
+								<!--mini cart end-->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="horizontal_menu">
+				<div class="left_menu">
+					<div class="main_menu">
+						<nav>
+							<ul>
+								<li class="active">
+									<a href="/">Home <i class="fa fa-angle-down" /></a>
+									<ul class="sub_menu">
+										<li><a href="/">Home 1</a></li>
+										<li><a href="index-2.html">Home 2</a></li>
+										<li><a href="index-3.html">Home 3</a></li>
+										<li><a href="index-4.html">Home 4</a></li>
+										<li><a href="index-5.html">Home 5</a></li>
+										<li><a href="index-6.html">Home 6</a></li>
+										<li><a href="index-7.html">Home 7</a></li>
+										<li><a href="index-8.html">Home 8</a></li>
+									</ul>
+								</li>
+								<li class="mega_items">
+									<a href="shop.html">shop <i class="fa fa-angle-down" /></a>
+									<ul class="mega_menu">
+										<li>
+											<a href="#">Shop Layouts</a>
+											<ul>
+												<li><a href="shop-fullwidth.html">Full Width</a></li>
+												<li><a href="shop-fullwidth-list.html">Full Width list</a></li>
+												<li><a href="shop-right-sidebar.html">Right Sidebar </a></li>
+												<li><a href="shop-right-sidebar-list.html"> Right Sidebar list</a></li>
+												<li><a href="shop-list.html">List View</a></li>
+											</ul>
+										</li>
+										<li>
+											<a href="#">other Pages</a>
+											<ul>
+												<li><a href="portfolio.html">portfolio</a></li>
+												<li><a href="portfolio-details.html">portfolio details</a></li>
+												<li><a href="cart.html">cart</a></li>
+												<li><a href="checkout.html">Checkout</a></li>
+												<li><a href="my-account.html">my account</a></li>
+											</ul>
+										</li>
+										<li>
+											<a href="#">Product Types</a>
+											<ul>
+												<li><a href="product-details.html">product details</a></li>
+												<li><a href="product-sidebar.html">product sidebar</a></li>
+												<li><a href="product-gallery.html">product gallery</a></li>
+												<li><a href="product-grouped.html">product grouped</a></li>
+												<li><a href="variable-product.html">product variable</a></li>
+											</ul>
+										</li>
+										<li>
+											<a href="#">collection</a>
+											<ul>
+												<li><a href="shop.html">Handbag</a></li>
+												<li><a href="shop.html">Accessories</a></li>
+												<li><a href="shop.html">Clothing</a></li>
+												<li><a href="shop.html">Shoes</a></li>
+												<li><a href="shop.html">Check Trousers</a></li>
+											</ul>
+										</li>
+										<li class="banner_menu">
+											<a href="#"><img src="assets/img/bg/banner1.jpg" alt="" /></a>
+										</li>
+									</ul>
+								</li>
+								<li>
+									<a href="blog.html">blog <i class="fa fa-angle-down" /></a>
+									<ul class="sub_menu pages">
+										<li><a href="blog-details.html">blog details</a></li>
+										<li><a href="blog-sidebar.html">blog Sidebar</a></li>
+										<li><a href="blog-fullwidth.html">blog fullwidth</a></li>
+									</ul>
+								</li>
+								<li>
+									<a href="#">pages <i class="fa fa-angle-down" /></a>
+									<ul class="sub_menu pages">
+										<li><a href="about.html">About Us</a></li>
+										<li><a href="services.html">services</a></li>
+										<li><a href="faq.html">Frequently Questions</a></li>
+										<li><a href="login.html">login</a></li>
+										<li><a href="my-account.html">my account</a></li>
+										<li><a href="wishlist.html">Wishlist</a></li>
+										<li><a href="404.html">Error 404</a></li>
+										<li><a href="compare.html">compare</a></li>
+										<li><a href="privacy-policy.html">privacy policy</a></li>
+										<li><a href="coming-soon.html">coming soon</a></li>
+									</ul>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+				<div class="logo_container">
+					<a href="/"><img class="logo2" src="assets/img/clothigs.png" alt="" /></a>
+				</div>
+				<div class="right_menu">
+					<div class="main_menu">
+						<nav>
+							<ul>
+								<li><a href="#">Specials</a></li>
+								<li><a href="#">Sneaker</a></li>
+								<li><a href="about.html">About us</a></li>
+								<li><a href="contact.html">Contact Us</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--header middel end-->
+
+	<!--header bottom satrt-->
+	<div class="header_bottom sticky-header">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-12">
+					<div class="main_menu_inner">
+						<div class="main_menu">
+							<nav>
+								<ul>
+									<li class="active">
+										<a href="/">Home <i class="fa fa-angle-down" /></a>
+										<ul class="sub_menu pages">
+											<li><a href="/">Home 1</a></li>
+											<li><a href="index-2.html">Home 2</a></li>
+											<li><a href="index-3.html">Home 3</a></li>
+											<li><a href="index-4.html">Home 4</a></li>
+											<li><a href="index-5.html">Home 5</a></li>
+											<li><a href="index-6.html">Home 6</a></li>
+											<li><a href="index-7.html">Home 7</a></li>
+											<li><a href="index-8.html">Home 8</a></li>
+											<li><a href="index-9.html">Home 9</a></li>
+										</ul>
+									</li>
+									<li class="mega_items">
+										<a href="shop.html">shop <i class="fa fa-angle-down" /></a>
+										<ul class="mega_menu">
+											<li>
+												<a href="#">Shop Layouts</a>
+												<ul>
+													<li><a href="shop-fullwidth.html">Full Width</a></li>
+													<li><a href="shop-fullwidth-list.html">Full Width list</a></li>
+													<li><a href="shop-right-sidebar.html">Right Sidebar </a></li>
+													<li><a href="shop-right-sidebar-list.html"> Right Sidebar list</a></li>
+													<li><a href="shop-list.html">List View</a></li>
+												</ul>
+											</li>
+											<li>
+												<a href="#">other Pages</a>
+												<ul>
+													<li><a href="portfolio.html">portfolio</a></li>
+													<li><a href="portfolio-details.html">portfolio details</a></li>
+													<li><a href="cart.html">cart</a></li>
+													<li><a href="checkout.html">Checkout</a></li>
+													<li><a href="my-account.html">my account</a></li>
+												</ul>
+											</li>
+											<li>
+												<a href="#">Product Types</a>
+												<ul>
+													<li><a href="product-details.html">product details</a></li>
+													<li><a href="product-sidebar.html">product sidebar</a></li>
+													<li><a href="product-gallery.html">product gallery</a></li>
+													<li><a href="product-grouped.html">product grouped</a></li>
+													<li><a href="variable-product.html">product variable</a></li>
+												</ul>
+											</li>
+											<li>
+												<a href="#">collection</a>
+												<ul>
+													<li><a href="shop.html">Handbag</a></li>
+													<li><a href="shop.html">Accessories</a></li>
+													<li><a href="shop.html">Clothing</a></li>
+													<li><a href="shop.html">Shoes</a></li>
+													<li><a href="shop.html">Check Trousers</a></li>
+												</ul>
+											</li>
+											<!-- svelte-ignore a11y-invalid-attribute -->
+											<li class="banner_menu">
+												<a href="#"><img src="assets/img/bg/banner1.jpg" alt="" /></a>
+											</li>
+										</ul>
+									</li>
+									<li>
+										<a href="blog.html">blog <i class="fa fa-angle-down" /></a>
+										<ul class="sub_menu pages">
+											<li><a href="blog-details.html">blog details</a></li>
+											<li><a href="blog-sidebar.html">blog Sidebar</a></li>
+											<li><a href="blog-fullwidth.html">blog fullwidth</a></li>
+										</ul>
+									</li>
+									<!-- svelte-ignore a11y-invalid-attribute -->
+									<li>
+										<a href="javascript:void()">pages <i class="fa fa-angle-down" /></a>
+										<ul class="sub_menu pages">
+											<li><a href="about.html">About Us</a></li>
+											<li><a href="services.html">services</a></li>
+											<li><a href="faq.html">Frequently Questions</a></li>
+											<li><a href="login.html">login</a></li>
+											<li><a href="my-account.html">my account</a></li>
+											<li><a href="wishlist.html">Wishlist</a></li>
+											<li><a href="404.html">Error 404</a></li>
+											<li><a href="compare.html">compare</a></li>
+											<li><a href="privacy-policy.html">privacy policy</a></li>
+											<li><a href="coming-soon.html">coming soon</a></li>
+										</ul>
+									</li>
+									<li><a href="about.html">About us</a></li>
+									<li><a href="contact.html">Contact Us</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--header bottom end-->
+</header>
+
+
+<style>
+    .logo2{
+        max-width: 40%;
+    height: auto;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Header from "$lib/components/Header.svelte";
+	import { onMount } from "svelte";
+	import Offcanvas from "../lib/components/Offcanvas.svelte";
+    let win: any ={};
+
+</script>
+<div class="off_canvars_overlay">
+                
+</div>
+<Offcanvas></Offcanvas>
+<Header></Header>
